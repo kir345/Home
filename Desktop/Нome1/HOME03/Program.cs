9c8510f3f7f28c4a10e3d8ba7abcdc8d7a9cf910
Kirill <kirill98761@rambler.ru>
2023-02-16 13:38:22 +0500

    Решение

---
+++ Desktop/Нome1/HOME03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num1 = 22;
+int num2 = 3;
+int num3 = 9;
+
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+
+if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("Наибольшее из введенных чисел -> " + max);
