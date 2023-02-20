23e2ce045a3286ace59a24e3d9a4e53698c8187d
Kirill <kirill98761@rambler.ru>
2023-02-20 19:35:43 +0500

    Задача2 исправление

---
+++ Desktop/Home02/2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int b = Int32.Parse(Console.ReadLine());
+if (b <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+     return;
+}
+else {
+    while (b > 999)
+    {
+        b /= 10;
+    }
+}
+Console.WriteLine(b%10);
+   
+
