d9ab1b53027fd8d0661b133892f4f94564716f51
kir345 <123649165+kir345@users.noreply.github.com>
2023-02-22 22:16:33 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
-﻿// Написать программу, которая принимает на вход пятизначное число и проверяет, является 
-// ли оно палиндроном.
-// 14212 -> нет
-// 12821 -> да 
-// 23432 -> да
+﻿// Написать программу, которая принимает на вход координаты двух точек и находит расстояние между 
+// ними в 3D пространстве.
+// А(3,6,8); В(2,1,-7) -> 15.84
+// A(7,-5,0); B(1,-1,9) -> 11.53
 
 Console.Clear();
-Console.WriteLine("Введите число: ");
-string num = Console.ReadLine();
-int len = num.Length;
+Console.WriteLine("");
 
-if (len == 5)
+int X1 =  ReadInt("Введите координату X первой точки: ");
+int Y1 = ReadInt("Введите координату Y первой точки: ");
+int Z1 = ReadInt("Введите координату Z первой точки: ");
+int X2 = ReadInt("Введите координату X второй точки: ");
+int Y2 = ReadInt("Введите координату Y второй точки: ");
+int Z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = X2 - X1;
+int B = Y2 - Y1;
+int C = Z2 - Z1;
+
+double  legth =  Math.Sqrt(A  * A  + B  * B  + C  *C);
+Console.WriteLine($"Длина отрезка {legth}");
+
+// Функция ввода сообщения
+int ReadInt(string message)
 {
-    if(num[0] == num[4] && num[1] == num[3])
-    {
-        Console.WriteLine($"{num} = да");
-    }
-else 
-    {
-        Console.WriteLine($"{num} = нет");
-    }
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
