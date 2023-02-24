b0963e8d4273e78d3db74fdce18197c9aa09be74
Kirill <kirill98761@rambler.ru>
2023-02-24 16:19:17 +0500

    Сумма цифр в числе

---
+++ Desktop/Home4/Home5/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+int num = ReadInt("Введите число: ");
+int len = NumberLen(num);
+SumNumbers (num, len);
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumberLen(int s)
+{
+    int index = 0;
+    while (s > 0)
+    {
+        s /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int a, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += a%10;
+        a /= 10;
+    }
+    Console.WriteLine(sum);
+}
